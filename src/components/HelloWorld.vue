04a4421489d2246efc2d5bf0b99d9454be8c376b
sourav-kumar123 <developertyagi1203@gmail.com>
2022-08-19 17:30:11 +0530

    todo app

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,174 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div>
+
+		<v-container>
+			<v-row justify="space-between">
+				<v-col
+					cols="12"
+					md="4"
+				>
+					<v-form v-on:submit.prevent>
+						<v-text-field
+							v-model="name"
+							type="text"
+							label="Name"
+							required
+						></v-text-field>
+
+						<v-btn
+							v-on:click="submit()"
+							color="success"
+							class="mr-4"
+							@keyup.13="submit()"
+						>
+							submit
+						</v-btn>
+
+					</v-form>
+				</v-col>
+			</v-row>
+		</v-container>
+
+		<v-container>
+			<v-row justify="space-between">
+				<v-col
+					cols="12"
+					md="4"
+				>
+
+					<v-list-item
+						v-for="(item, index) in list"
+						:key="index"
+						:value="item"
+					>
+
+						<v-list-item-content style="border:2px solid black">
+							<v-row>
+								<v-col
+									cols="3"
+									:class="{ hide:item.iscompleted  }"
+								>
+
+									{{ item.name }} ,{{item.timestamp }}
+								</v-col>
+
+								<v-col cols="3">
+									<v-btn
+										@click="completed(index)"
+										color="success"
+										class="mr-4"
+									>
+										Completed
+									</v-btn>
+								</v-col>
+								<v-col cols="3">
+									<v-btn
+										v-on:click="Edit(item, index)"
+										color="success"
+										class="mr-1"
+										:disabled="item.iscompleted == true"
+									>
+										Edit
+									</v-btn>
+								</v-col>
+								<v-col cols="3">
+									<v-btn
+										v-on:click="close(index)"
+										color="success"
+										class="mr-4"
+									>
+										Delete
+									</v-btn>
+								</v-col>
+							</v-row>
+						</v-list-item-content>
+					</v-list-item>
+
+				</v-col>
+			</v-row>
+		</v-container>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+	export default {
+		name: "HelloWorld",
+		data() {
+			return {
+				hidden: false,
+				name: "",
+				list: [],
+				isEdit: null,
+				disabled: "",
+				arr: [],
+			};
+		},
+		methods: {
+			submit( ) {
+				const today = new Date();
+				this.monthNames = [
+					"January",
+					"February",
+					"March",
+					"April",
+					"May",
+					"June",
+					"July",
+					"August",
+					"September",
+					"October",
+					"November",
+					"December",
+				];
+				this.timestamp =
+					today.getDate() +
+					" " +
+					this.monthNames[today.getMonth()] +
+					"," +
+					today.getHours() +
+					":" +
+					today.getMinutes() +
+					":" +
+					today.getSeconds();
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+				if (this.name.trim()) {
+					let obj = {
+						name: this.name,
+						iscompleted: this.iscompleted,
+						timestamp: this.timestamp,
+					};
+
+					
+						this.list.push(obj);
+					
+					this.name = "";
+					this.isEdit = null;
+					this.timestamp = "";
+					this.iscompleted=false;
+				}
+				
+			},
+			close(index) {
+				{
+					this.list.splice(index, 1);
+				}
+				this.name = "";
+			},
+			Edit(item, index) {
+				this.isEdit = index;
+				this.name = item.name;
+				
+				
+			},
+			completed( index) {
+				this.list[index].iscompleted=true;
+								
+			},
+		},
+	};
+</script>
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.hide {
+	text-decoration-line: line-through;
 }
 </style>
